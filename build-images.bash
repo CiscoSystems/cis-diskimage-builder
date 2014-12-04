e5e23edf142c4f4c0aa4a552a7b273540e3d98e1
Abel Lopez <abelopez@cisco.com>
2014-12-04 11:30:51 -0800

    new rhel7 registration
    Now requires to actually regiester/unregister at build time
    
    Change-Id: I7c877ce7126c54af454ded0fcdbd519032ca8c4b

--- build-images.bash
+++ build-images.bash
@@ -12,6 +12,8 @@ disk-image-create -a amd64 -o centos7.qcow2 centos7 base vm cisco
 # This is a lame assumption that I'm trying to work around
 export DIB_CLOUD_IMAGES="http://173.39.232.144/images/"
 export BASE_IMAGE_FILE="rhel-guest-image-7.0-20140618.1.x86_64.qcow2"
+export REG_METHOD='portal'
+export REG_AUTO_ATTACH=1
 disk-image-create -a amd64 -o rhel7.qcow2 rhel7 base vm cisco
 
 
